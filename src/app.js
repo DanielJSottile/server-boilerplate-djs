57208b439306b07b08516389c0c96ba6b71b7f5c
Daniel Sottile <danielsottile@gmail.com>
2020-04-30 10:38:52 -0400

    added eslint ignores for console log on server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
@@ -11,9 +12,19 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+// middleware
+
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+// server requests
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// errorHandler middleware
 
 app.use(function errorHandler(error, req, res, next){
   let response;
@@ -26,9 +37,6 @@ app.use(function errorHandler(error, req, res, next){
   res.status(500).json(response);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
+// exports
 
 module.exports = app;
