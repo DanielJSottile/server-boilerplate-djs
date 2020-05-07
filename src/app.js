9f345f56a149c9af050b13b41887657ee727a089
Daniel Sottile <danielsottile@gmail.com>
2020-05-07 11:19:37 -0400

    updated to work with databases

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const Service = require('./service');
 const logger = require('./logger');
+const { NODE_ENV } = require('./config');
+
 
 const app = express();
 
